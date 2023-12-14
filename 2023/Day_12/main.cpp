2942e48b52cd9cd9463714c1db5296a23bedca7c
btitus1 <btitus1@asu.edu>
2023-12-13 16:33:34 -0800

    Updated

--- 2023/Day_12/main.cpp
+++ 2023/Day_12/main.cpp
@@ -1,146 +1,173 @@
-#include <iostream>
-#include <vector>
-#include <string>
+/*
+ * Copyright © 2023 Brian Titus
+ * Contact: realcookiemonster69@gmail.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files(the “Software”), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and /or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// Advent of Code Day 12
+// https://adventofcode.com/2023/day/12
+
+#include <cassert>
 #include <fstream>
-#include <algorithm>
+#include <iostream>
 #include <map>
-#include <cstdint>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
 
 using namespace std;
-constexpr int none = -1;
 
-uint64_t collect(
-        const string &grid,
-        int i_grid,
-        const vector<int> &sizes,
-        int i_size,
-        int remain,
-        map<tuple<int, int, int>, uint64_t> &table) {
-    const auto key = make_tuple(i_grid, i_size, remain);
-    const auto found = table.find(key);
-    if (found != table.cend()) { return found->second; }
-    char ch;
-    while (i_size != sizes.size() && i_grid != grid.size() && '?' != (ch = grid[i_grid])) {
-        ++i_grid;
-        if (ch == '.') {
-            if (remain == none) {
-                // do nothing
-            } else if (remain == 0) {  // just finished a batch of #
-                ++i_size;
-                remain = none;
-            } else {  // remain > 0
-                table[key] = 0;
-                return 0;  // impossible, so terminate
-            }
-        } else {  // ch == '#'
-            if (remain == none) {
-                remain = sizes[i_size] - 1;
-            } else if (remain == 0) {
-                table[key] = 0;
-                return 0;  // impossible
-            } else {  // remain > 0
-                --remain;
-            }
+const string file1 = "input.txt";
+
+static bool readFile(const string &fileName, vector<string> &lines) {
+    ifstream in(fileName);
+    if (!in) {
+        cerr << "Cannot open file " << fileName << endl;
+        return false;
+    }
+
+    auto closeStream = [&in] {
+        in.close();
+    };
+
+    string str;
+    while (getline(in, str)) {
+        lines.push_back(str);
+    }
+
+    closeStream();
+    return true;
+}
+
+using Group = vector<uint32_t>;
+using Puzzle = pair<string, Group>;
+using Puzzles = vector<Puzzle>;
+
+template <size_t FOLD = 1, bool WITH_CACHE = true>
+static size_t count(const string& p, const Group& g, size_t pos = 0, size_t k = 0,
+                    shared_ptr<map<pair<size_t, size_t>, size_t> > cache = nullptr)
+{
+    if constexpr (WITH_CACHE) {
+        assert(nullptr != cache);
+        if (const auto it = cache->find({pos, k}); it != cache->end()) {
+            return it->second;
         }
     }
-    if (i_size == sizes.size()) {
-        for (int i = i_grid; i < grid.size(); ++i) {
-            if (grid[i] == '#') {
-                table[key] = 0;
+    if (pos == p.size() && k == FOLD * g.size()) {
+        return 1;
+    }
+    if (pos >= p.size()) {
+        return 0;
+    }
+    if ('#' == p[pos]) {
+        if (k >= FOLD * g.size() || pos + g[k % g.size()] >= p.size()) {
+            return 0;
+        }
+        for (size_t c = 1; c < g[k % g.size()]; ++c) {
+            if ('.' == p[pos + c]) {
                 return 0;
             }
         }
-        table[key] = 1;
-        return 1;
-    } else if (i_grid == grid.size()) {
-        if (remain == 0 && i_size + 1 == sizes.size()) {
-            table[key] = 1;
-            return 1;
+        pos += g[k % g.size()];
+        k++;
+        if ('#' == p[pos]) {
+            return 0;
         }
-    } else {  // ch == '?' and haven't finished yet
-        // try ? = #
-        uint64_t total = 0;
-        if (remain == none) {
-            const auto new_remain = sizes[i_size] - 1;
-            total += collect(grid, 1 + i_grid, sizes, i_size, new_remain, table);
-        } else if (remain > 0) {
-            total += collect(grid, 1 + i_grid, sizes, i_size, remain - 1, table);
+        const auto s = count<FOLD, WITH_CACHE>(p, g, pos + 1, k, cache);
+        if constexpr (WITH_CACHE) {
+            cache->insert({{pos + 1, k}, s});
         }
-        // try ? = .
-        if (remain == 0) {
-            total += collect(grid, 1 + i_grid, sizes, 1 + i_size, none, table);
-        } else if (remain == none) {
-            total += collect(grid, 1 + i_grid, sizes, i_size, none, table);
-        }
-        table[key] = total;
-        return total;
+        return s;
     }
-    table[key] = 0;
-    return 0;
+    if ('.' == p[pos]) {
+        return count<FOLD, WITH_CACHE>(p, g, pos + 1, k, cache);
+    }
+    const string p1 = p.substr(0, pos) + '#' + p.substr(pos + 1, p.size() - pos - 1);
+    const string p2 = p.substr(0, pos) + '.' + p.substr(pos + 1, p.size() - pos - 1);
+    return count<FOLD, WITH_CACHE>(p1, g, pos, k, cache) + count<FOLD, WITH_CACHE>(p2, g, pos, k, cache);
 }
 
-uint64_t solve(const string &grid, const vector<int> &sizes) {
-    map<tuple<int, int, int>, uint64_t> table;
-    return collect(grid, 0, sizes, 0, none, table);
+static inline size_t count1(const string& p, const Group& g)
+{
+    return count<1, false>(p, g);
 }
 
-struct Problem {
-    string grid;
-    vector<int> nums;
-};
-
-auto split_nums(const string &list) {
-    size_t i = 0;
-    vector<int> nums;
-    while (true) {
-        const auto k = list.find(',', i);
-        if (k == string::npos) {
-            nums.push_back(stoi(list.substr(i)));
-            break;
-        } else {
-            nums.push_back(stoi(list.substr(i, k - i)));
-            i = k + 1;
-        }
-    }
-    return nums;
-}
-
-auto times_five_with_questions(const string &text) {
-    string out = text;
-    for (int i = 0; i < 4; ++i) {
-        out.push_back('?');
-        copy(text.cbegin(), text.cend(), back_inserter(out));
-    }
-    return out;
-}
-
-auto times_five(const vector<int> &sizes) {
-    vector<int> out = sizes;
-    for (int i = 0; i < 4; ++i) {
-        copy(sizes.cbegin(), sizes.cend(), back_inserter(out));
-    }
-    return out;
+static inline size_t count5(const string& p, const Group& g)
+{
+    return count<5>(p, g, 0, 0, make_shared<map<pair<size_t, size_t>, size_t> >());
 }
 
 int main() {
-    vector<Problem> problems;
-    if (auto fs = ifstream("input.txt")) {
-        cout << "Day 9" << endl;
+    cout << "Day 12" << endl;
 
-        uint64_t part1 = 0, part2 = 0;
-        string grid, nums;
-        while (fs >> grid) {
-            fs >> nums;
-            const auto sizes = split_nums(nums);
-            part1 += solve(grid, sizes);
-            const auto five_grid = times_five_with_questions(grid);
-            const auto five_sizes = times_five(sizes);
-            part2 += solve(five_grid, five_sizes);
+    vector<string> lines{};
+    if (!readFile(file1, lines)) {
+        return EXIT_FAILURE;
+    }
+
+    {  // Part 1
+        Puzzles puzzles;
+        for (const auto& line : lines) {
+            uint32_t x;
+            string p;
+            istringstream iss{line};
+            iss >> p;
+            p += '.';
+            Group g;
+            while (iss >> x) {
+                g.push_back(x);
+                iss.ignore(1);
+            }
+            puzzles.emplace_back(p, g);
+        }
+
+        size_t sum{};
+        for (const auto& [p, g] : puzzles) {
+            sum += count1(p, g);
         }
         cout << "  Part 1" << endl;
-        cout << "     Sum of counts : " << part1 << endl;
-
-        cout << "  Part 2" << endl;
-        cout << "     New sum of counts : " << part2 << endl;
+        cout << "     Sum of possible arrangements : " << sum << endl;
     }
+    {  // Part 2
+        Puzzles puzzles;
+        for (const auto& line : lines) {
+            uint32_t x;
+            string p;
+            istringstream iss{line};
+            iss >> p;
+            p = p + '?' + p + '?' + p + '?' + p + '?' + p + '.';
+            Group g;
+            while (iss >> x) {
+                g.push_back(x);
+                iss.ignore(1);
+            }
+            puzzles.emplace_back(p, g);
+        }
+
+        size_t sum{};
+        for (const auto& [p, g] : puzzles) {
+            sum += count5(p, g);
+        }
+        cout << "  Part 2" << endl;
+        cout << "     Sum of possible arrangements : " << sum << endl;
+    }
+
+    return EXIT_SUCCESS;
 }
